687126b857df0ff42f05d40f87a4dda8ce7e371e
tarosay <tarosa.y@gmail.com>
2017-05-06 02:23:16 +0900

    test commit

--- source/getRUL/main.rb
+++ source/getRUL/main.rb
@@ -9,14 +9,43 @@ digitalWrite(5,1)   # LOW:Disable
 
 Usb = Serial.new(0,115200)
 
-if( System.useWiFi() == 0)then
-    Usb.println "WiFi Card can't use."
+if( !System.use?('SD') || !System.use?('WiFi'))then
+    Usb.println "SD Card or WiFi can't use."
    System.exit() 
 end
-Usb.println "WiFi Ready"
+Usb.println "SD & WiFi Ready"
 
-#Usb.println "WiFi Get Version"
-#Usb.println WiFi.version
+#-------
+#ボディを取り出します
+#-------
+def getBody(bodyname, tmpname)
+  SD.open(0, tmpname, 0)
+  c = SD.read(0)
+  crlf = 0
+  while(c >= 0)do
+    if(c == 0xd || c == 0xa)then
+      crlf += 1
+      if(crlf == 4)then
+          break
+      end
+    else
+      crlf = 0
+    end
+    c = SD.read(0)
+  end
+  
+  SD.open(1, bodyname, 2)
+  while(c >= 0)do
+    Usb.write(c.chr, 2)
+    SD.write(1, c.chr, 1)
+    c = SD.read(0)
+  end
+  SD.close(1)
+  SD.close(0)
+end
+
+#-----------------------------------------
+Usb.println("System Start")
 
 WiFi.disconnect
 WiFi.setMode 3  #Station-Mode & SoftAPI-Mode
@@ -25,9 +54,8 @@ WiFi.connect("TAROSAY","37000")
 WiFi.multiConnect 1
 
 Usb.println "HTTP GET Start"
-res = WiFi.httpGetSD("geturl1.tmp","github.com:443/tarosay/iotlt/blob/master/docs/index.html")
-res = WiFi.httpGetSD("geturl2.tmp","github.com/tarosay/iotlt/blob/master/docs/index.html")
-#res = WiFi.httpGetSD("main.tmp","www.geocities.jp/momoonga/iot/geturl.txt")
+res = WiFi.httpGetSD("geturl.tmp","tarosay.github.io/iotdl/data/geturl.txt")
+#res = WiFi.httpGetSD("geturl.tmp","tarosay.github.io/iotdl/data/main.mrb")
 WiFi.disconnect
 
 if(res != 1)then
@@ -36,4 +64,6 @@ if(res != 1)then
   System.reset
   System.exit
 end
-    
+
+#取得するmrbファイルのURLを取り出します    
+getBody("geturl.txt", "geturl.tmp")

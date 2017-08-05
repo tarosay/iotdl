9462b72b05d3cb5d1fb4aff1cd1575f5a08963b4
tarosay <tarosa.y@gmail.com>
2017-08-06 02:28:16 +0900

    2017/8/6 2:26

--- source/autocar/autocar.rb
+++ source/autocar/autocar.rb
@@ -119,7 +119,7 @@ while true do
   5.times do
     delay 50
     #Usb.println analogRead(Sens).to_s
-    if(analogRead(Sens) > 475)then
+    if(analogRead(Sens) > 450)then
       #ランダムで右か左回転する
       ro = random(2)
       if ro == 0 then

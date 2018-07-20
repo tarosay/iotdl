9804a46015db44e706018250b5491aa61e4ea742
tarosay <tarosa.y@gmail.com>
2018-07-21 03:45:53 +0900

    2018/7/21 3:44

---
+++ source/ntp/ntp.rb
@@ -0,0 +1,40 @@
+#!mruby
+#GR-CITRUS Version 2.50
+#ESP8266を一度停止させる(リセットと同じ)
+WiFiEN = 5          #WiFiのEN:LOWでDisableです
+
+pinMode(WiFiEN, OUTPUT)
+digitalWrite(WiFiEN, LOW)     # LOW:Disable
+delay 500
+digitalWrite(WiFiEN ,HIGH)    # HIGH:Enable
+delay 500
+
+System.exit "WA-MIKAN can't use." if(!System.use?("WiFi"))
+
+puts WiFi.connect("TAROSAY","37000370")
+tm = WiFi.ntp("ntp.jst.mfeed.ad.jp", +9)
+
+System.exit "時間の取得に失敗しました" if(!tm.instance_of?(Array))
+
+#puts tm
+Rtc.init
+Rtc.setTime(tm)
+
+def zeroAdd(num)
+  str = "00" + num.to_s
+  str[str.length-2..str.length]
+end
+
+#wifiload.mrbをmain.mrbとして上書きコピー
+puts "wifiload.mrb を main.mrb にコピー中"
+MemFile.cp("wifiload.mrb", "main.mrb", 1)
+
+loop do
+#60.times do
+  tm = Rtc.getTime
+  #puts tm
+  body = tm[0].to_s + "/" + zeroAdd(tm[1]) + "/" + zeroAdd(tm[2])
+  body += " " + zeroAdd(tm[3]) + ":" +zeroAdd(tm[4]) + ":" +zeroAdd(tm[5])
+  puts body
+  delay 1000
+end
